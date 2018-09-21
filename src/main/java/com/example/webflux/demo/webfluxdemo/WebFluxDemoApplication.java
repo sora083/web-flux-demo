c61ca72cd17bfbe5a4ec27e4f42c27f5fa28c707
sora.tomita <sora.tomita@rakuten.com>
2018-09-21 12:04:11 +0900

    add router function

--- src/main/java/com/example/webflux/demo/webfluxdemo/WebFluxDemoApplication.java
+++ src/main/java/com/example/webflux/demo/webfluxdemo/WebFluxDemoApplication.java
@@ -2,11 +2,23 @@ package com.example.webflux.demo.webfluxdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class WebFluxDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFluxDemoApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routes() {
+		return RouterFunctions.route(RequestPredicates.GET("/")
+				, req -> ServerResponse.ok().body(Flux.just("Hello", "World!"), String.class));
 	}
 }
